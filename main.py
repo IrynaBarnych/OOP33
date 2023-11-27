54879827189bd0ca2913ce8e299f34e1553a111a
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-27 20:43:15 +0200

    заміна

--- main.py
+++ main.py
@@ -6,46 +6,6 @@ class Node:
 
     def __str__(self):
         return f"[{self.data}] -> {self.next}"
-node1 = Node(1)
-print(node1)
-node2 = Node(2)
-print(node2)
-node3 = Node(3)
-node1.next = node2
-print(node1)
-node1.next = node2
-node2.next = node3
-print(node1)
-
-#
-
-#Синтаксис Однозв'язковий список
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-    def __str__(self):
-        return f"[{self.data}] -> {self.next}"
-
-node = Node(1)
-current_node = node
-
-for i in range(2, 330):
-    new_node = Node(i)
-    current_node.next = new_node
-    current_node = new_node
-
-print(node)
-
-#Синтаксис Однозв'язковий список
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-    def __str__(self):
-        return f"[{self.data}] -> {self.next}"
 
 class LinkedList:
     def __init__(self):
@@ -63,10 +23,25 @@ class LinkedList:
         while last_node.next:
             last_node = last_node.next #переміщуємося до наступного елементу
         last_node.next = new_node
-my_lst = LinkedList()
-my_lst.append(1)
-my_lst.append(2)
-my_lst.append(3)
-my_lst.append(4)
-print(my_lst)
 
+    def replace(self, old_data, new_data):
+        current = self.head
+        while current:
+            if current.data == old_data:
+                current.data = new_data
+                print(f"Значення {old_data} замінено на {new_data}")
+                return
+            current = current.next
+        print(f"Значення {old_data} не знайдено у списку")
+
+my_lst = LinkedList()
+nums = input("Введіть усі числа через пробіл ").split()
+for num in nums:
+    my_lst.append(int(num))
+print(my_lst)
+#сюди пункти меню
+#Замінити значення у списку
+old_value = int(input("Введіть значення, яке треба замінити: "))
+new_value = int(input("Введіть значення, на яке замінити: "))
+my_lst.replace(old_value, new_value)
+print(my_lst)
